22ea1163e9706850ee62bacc373d8b13e36d6fb8
unknown <adebunmiola1@gmail.com>
2024-03-09 15:37:42 -0100

    firstCommit

---
+++ main.js
@@ -0,0 +1,30 @@
+function arrowFirst(){
+    document.querySelector("#define").style.display= "block"
+    document.querySelector("#firstbutton").style.display="none"
+    
+}
+function secondArrow(){
+    document.querySelector("#define").style.display= "none"
+    document.querySelector("#secondtbutton").style.display="block"
+    document.querySelector("#firstbutton").style.display="block"
+
+}
+
+function FirstArrow(){
+    document.querySelector("#define2").style.display= "block"
+    document.querySelector("#firstbutton1").style.display="none"
+}
+function secondArrow1(){
+    document.querySelector("#define2").style.display= "none"
+    document.querySelector("#secondbutton2").style.display="block"
+    document.querySelector("#firstbutton1").style.display="block" 
+}
+function arrowDown(){
+    document.querySelector("#basiclist").style.display= "block"
+    document.querySelector("#arrowdown").style.display="none"
+}
+function arrowUp(){
+    document.querySelector("#basiclist").style.display= "none"
+    document.querySelector("#arrowup").style.display="block"
+    document.querySelector("#arrowdown").style.display="block" 
+}
